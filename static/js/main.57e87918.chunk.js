ad1ad4f6a5cf21abb64a247b8de8d178974f499c
Igor <polygor@gmail.com>
2021-12-26 18:29:17 +0200

    Updates

--- static/js/main.57e87918.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-02-phonebook"]=this["webpackJsonpgoit-react-hw-02-phonebook"]||[]).push([[0],{10:function(e,t,o){"use strict";o.r(t);var n=o(1),c=o.n(n),r=o(3),i=o.n(r),h=(o(8),o(0));var s=function(){return Object(h.jsx)("div",{className:"App",children:Object(h.jsx)("h1",{children:"goit-react-hw-02-phonebook"})})};i.a.render(Object(h.jsx)(c.a.StrictMode,{children:Object(h.jsx)(s,{})}),document.getElementById("root"))},8:function(e,t,o){}},[[10,1,2]]]);
-//# sourceMappingURL=main.57e87918.chunk.js.map
